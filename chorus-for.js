f35573d6dea0b6c536db935a6c41a5aa246a04d4
Lauren Hepburn <laurendawnhepburn@gmail.com>
2021-08-25 20:53:30 +0000

    chorus-for

--- chorus-for.js
+++ chorus-for.js
@@ -3,3 +3,18 @@ for (let repeat = 0; repeat < 10; repeat++) {
   console.log(chorus);
 }
 console.log("Until the sun comes up!");
+
+for (var x = 100; x <= 200; x++)
+{
+  switch(x){
+    case(x / 3 == 0 && x / 4 != 0): 
+      console.log("Loopy");
+      break;
+    case(x / 4 == 0 && x / 3 != 0):
+      console.log("Lighthouse");
+      break;
+    case(x / 3 == 0 && x / 4 == 0):
+      console.log("LoopyLighthouse");
+  }
+  console.log(x);
+}
---
+++ practice.js
@@ -0,0 +1,10 @@
+function whichSchool(age)
+{
+  if (age < 13)
+    console.log("Elementary School");
+  else if (age >= 13 && age <= 18)
+    console.log("secondary School");
+  else
+    console.log("lighthouse Labs");
+}
+
